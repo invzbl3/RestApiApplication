10ea22c2ab8ab54877053bd2f2fbd00117592201
Maksym Rohachov <romax982@gmail.com>
2022-12-15 10:45:47 +0100

    Update the logic of the project

--- src/main/java/com/test/application/exception/EntityNotFoundException.java
+++ src/main/java/com/test/application/exception/EntityNotFoundException.java
@@ -4,5 +4,11 @@ package com.test.application.exception;
  * @author invzbl3 on 12/15/2022
  * @project RestApiApplication
  */
-public class EntityNotFoundException {
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(){
+        super();
+    }
+    public EntityNotFoundException(String message){
+        super(message);
+    }
 }
