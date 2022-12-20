7f8c1a00f1947ba1e081830d954aa9cfe170d1da
Maksym Rohachov <romax982@gmail.com>
2022-12-20 12:29:51 +0100

    Update the logic of the project

--- src/main/java/com/test/application/repository/ProjectRepository.java
+++ src/main/java/com/test/application/repository/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.test.application.repository;
 
+import com.test.application.data.models.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 /**
  * @author invzbl3 on 12/20/2022
  * @project RestApiApplication
  */
 public interface ProjectRepository {
 
+    Page<Project> findAll(Pageable pageable);
 }
