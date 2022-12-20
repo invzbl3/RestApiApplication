68e885df5a763757dc0e3db817c4a97537c68c6c
Maksym Rohachov <romax982@gmail.com>
2022-12-20 21:24:02 +0100

    Update the logic of the project

--- src/main/java/com/test/application/dto/EmployeeDTO.java
+++ src/main/java/com/test/application/dto/EmployeeDTO.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import java.io.Serializable;
