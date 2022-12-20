c7f8249e5a2f6161c5226c183c7ef9b3e8eaf7b1
Maksym Rohachov <romax982@gmail.com>
2022-12-20 12:39:49 +0100

    Update the logic of the project

--- src/main/java/com/test/application/repository/ProjectRepository.java
+++ src/main/java/com/test/application/repository/ProjectRepository.java
@@ -3,6 +3,8 @@ package com.test.application.repository;
 import com.test.application.data.models.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author invzbl3 on 12/20/2022
@@ -11,4 +13,12 @@ import org.springframework.data.domain.Pageable;
 public interface ProjectRepository {
 
     Page<Project> findAll(Pageable pageable);
+
+    List<Project> findAll();
+
+    Project save(Project user);
+
+    Optional<Project> findById(Long id);
+
+    void deleteById(Long id);
 }
