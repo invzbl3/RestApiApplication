4f6dfa36aef031e22a2942ea380090f0334d9a8c
Maksym Rohachov <romax982@gmail.com>
2022-12-23 00:25:30 +0100

    Update the logic of the project

--- src/main/java/com/test/application/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/test/application/service/impl/ProjectServiceImpl.java
@@ -1,10 +1,30 @@
 package com.test.application.service.impl;
 
+import com.test.application.data.models.Project;
+import com.test.application.dto.ProjectDTO;
+import com.test.application.service.ProjectService;
+
+import java.util.List;
+
 /**
  * @author invzbl3 on 12/20/2022
  * @project RestApiApplication
  */
-public class ProjectServiceImpl {
+public class ProjectServiceImpl implements ProjectService {
 
 
+    @Override
+    public List<Project> getAllProjects(Long companyId) {
+        return null;
+    }
+
+    @Override
+    public Project save(Project project) {
+        return null;
+    }
+
+    @Override
+    public List<ProjectDTO> findAllProducts() {
+        return null;
+    }
 }
