5baf1345ea63fa686303ec3fc7b25d2e64af5f63
Maksym Rohachov <romax982@gmail.com>
2022-12-23 17:13:00 +0100

    Update the logic of the project

--- src/main/java/com/test/application/Application.java
+++ src/main/java/com/test/application/Application.java
@@ -1,12 +1,13 @@
 package com.test.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author invzbl3 on 12/15/2022
  * @project RestApiApplication
  */
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
