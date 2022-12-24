fb2f1a23f8abf4ca2a8960832e451e8fbbd9ac8c
Maksym Rohachov <romax982@gmail.com>
2022-12-25 00:01:02 +0100

    Update the instruction

--- src/main/java/com/test/application/Application.java
+++ src/main/java/com/test/application/Application.java
@@ -2,12 +2,14 @@ package com.test.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author invzbl3 on 12/15/2022
  * @project RestApiApplication
  */
 @SpringBootApplication
+@EnableWebMvc
 public class Application {
 
     public static void main(String[] args) {
