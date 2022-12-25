3b4b391845f5e776698ab26e80a69f5424978818
Maksym Rohachov <romax982@gmail.com>
2022-12-25 15:24:50 +0100

    Update the code

--- src/main/java/com/test/application/data/repository/UserRepository.java
+++ src/main/java/com/test/application/data/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.test.application.data.repository;
 
 import com.test.application.data.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author invzbl3 on 12/25/2022
  * @project RestApiApplication
  */
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
