82573d4a8a246d85981ffc7ea52f02d277d34d17
Maksym Rohachov <romax982@gmail.com>
2022-12-25 14:45:19 +0100

    Update the code

--- src/main/java/com/test/application/repository/ProjectRepository.java
+++ src/main/java/com/test/application/repository/ProjectRepository.java
@@ -1,14 +1,9 @@
 package com.test.application.repository;
 
 import com.test.application.data.models.Project;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author invzbl3 on 12/20/2022
