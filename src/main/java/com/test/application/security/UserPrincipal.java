a5e39550a3cb2b54615f4214c5df35ade5be4b94
Maksym Rohachov <romax982@gmail.com>
2022-12-25 15:21:34 +0100

    Update the code

--- src/main/java/com/test/application/security/UserPrincipal.java
+++ src/main/java/com/test/application/security/UserPrincipal.java
@@ -1,8 +1,64 @@
 package com.test.application.security;
 
+import com.test.application.data.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * @author invzbl3 on 12/25/2022
  * @project RestApiApplication
  */
-public class UserPrincipal {
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    public UserPrincipal(User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
 }
