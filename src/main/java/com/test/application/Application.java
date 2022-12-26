ea6a299da30011bd7088567beb054ca5a993392c
Maksym Rohachov <romax982@gmail.com>
2022-12-26 17:13:02 +0100

    Update the code

--- src/main/java/com/test/application/Application.java
+++ src/main/java/com/test/application/Application.java
@@ -3,14 +3,12 @@ package com.test.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author invzbl3 on 12/15/2022
  * @project RestApiApplication
  */
 @SpringBootApplication
-@EnableWebMvc
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
