2dcdc5f1d5fad1aafd01904832ad8ea7c4e811be
Maksym Rohachov <romax982@gmail.com>
2022-12-26 16:35:23 +0100

    Update the code

--- src/main/java/com/test/application/service/UserService.java
+++ src/main/java/com/test/application/service/UserService.java
@@ -3,12 +3,14 @@ package com.test.application.service;
 import com.test.application.data.models.User;
 import com.test.application.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 /**
  * @author invzbl3 on 12/26/2022
  * @project RestApiApplication
  */
+@Service
 public class UserService {
 
     @Autowired
